a4b710d864276527d18b5c6171aeaa4fc0e117d5
Valdir Coxev [Job] <valdircoxevdev@gmail.com>
2020-04-05 19:55:17 -0400

    Adding axios interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
+api.interceptors.response.use(
+  (response) => {
+    localStorage.setItem(
+      'x-ratelimit',
+      JSON.stringify({
+        remaining: response.headers['x-ratelimit-remaining'],
+        reset: response.headers['x-ratelimit-reset'],
+      }),
+    );
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default api;
