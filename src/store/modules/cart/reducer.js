0b004fcfed5eb85c9463005c54fe0828ac8c5dbc
Valdir Coxev [Job] <valdircoxevdev@gmail.com>
2020-04-05 19:58:37 -0400

    Cart reducer state add, update and delete item

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,28 @@
-export default function Cart() {
-  return [];
+import produce from 'immer';
+
+export default function Cart(state = [], action) {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case '@cart/ADD_SUCCESS': {
+        const { members } = action;
+        draft.push(members);
+        break;
+      }
+      case '@cart/REMOVE': {
+        const memberIndex = draft.findIndex((m) => m.id === action.id);
+        if (memberIndex >= 0) {
+          draft.splice(memberIndex, 1);
+        }
+        break;
+      }
+      case '@cart/UPDATE_AMOUNT_SUCCESS': {
+        const memberIndex = draft.findIndex((m) => m.id === action.id);
+        if (memberIndex >= 0) {
+          draft[memberIndex].amount = Number(action.amount);
+        }
+        break;
+      }
+      default:
+    }
+  });
 }
