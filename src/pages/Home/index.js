76459799f417efee166abde638b0ca9a599a7a4d
Valdir Coxev [Job] <valdircoxevdev@gmail.com>
2020-04-08 08:32:05 -0400

    Adding filter by price

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { MemberList } from './styles';
+import { MemberActions, Select, MemberList } from './styles';
 import api from '~/services/api';
 import * as CartActions from '~/store/modules/cart/actions';
 import { formatPrice } from '~/utils/formatCurrency';
+import { sortAsc, sortDesc } from '~/utils/filter';
 import Loading from '~/components/Loading';
 
 export default function Home() {
@@ -48,6 +49,16 @@ export default function Home() {
     dispatch(CartActions.addToCartRequest(item));
   }
 
+  function handleSortByInput(e) {
+    const { value } = e.target;
+    const direction = value.endsWith('asc') ? 'asc' : 'desc';
+    const sortedPrice =
+      direction === 'asc'
+        ? sortAsc(members, 'price')
+        : sortDesc(members, 'price');
+    setMember([...sortedPrice]);
+  }
+
   useEffect(() => {
     loadMembers();
   }, []);
@@ -55,21 +66,32 @@ export default function Home() {
   return loading ? (
     <Loading />
   ) : (
-    <MemberList>
-      {members.map((item) => (
-        <li key={item.id}>
-          <img src={item.avatar_url} alt={item.name} />
-          <strong>{item.name || item.login}</strong>
-          <span>{item.priceFormat}</span>
-          <button type="button" onClick={() => handleAddMember(item)}>
-            <div>
-              <FiShoppingCart size={16} color="#fff" />
-              {amount[item.id] || 0}
-            </div>
-            <span>Adicionar ao carrinho</span>
-          </button>
-        </li>
-      ))}
-    </MemberList>
+    <>
+      <MemberActions>
+        <Select onChange={(e) => handleSortByInput(e)} defaultValue="price">
+          <option disabled value="price">
+            Ordenar por
+          </option>
+          <option value="price_desc">Maior Preço</option>
+          <option value="price_asc">Menor Preço</option>
+        </Select>
+      </MemberActions>
+      <MemberList>
+        {members.map((item) => (
+          <li key={item.id}>
+            <img src={item.avatar_url} alt={item.name} />
+            <strong>{item.name || item.login}</strong>
+            <span>{item.priceFormat}</span>
+            <button type="button" onClick={() => handleAddMember(item)}>
+              <div>
+                <FiShoppingCart size={16} color="#fff" />
+                {amount[item.id] || 0}
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </MemberList>
+    </>
   );
 }
